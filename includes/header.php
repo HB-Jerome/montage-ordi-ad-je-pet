cb173216dff990c09968f78885ff570ff5f6472a
Peter-k <111885308+KOMUPeter@users.noreply.github.com>
2023-05-03 11:56:50 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,3 @@
 <?php
-session_start();
-require_once "includes/config.inc.php";
-require_once "includes/autoload.php";
-require_once "includes/variable.php";
-require_once "includes/function.php";
+
 ?>
