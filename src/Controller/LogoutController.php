06063befc60203f45139d89dec9478a4de296830
komu <komupeter16@yahoo.com>
2023-05-03 22:56:10 +0200

    m

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Controller;
+
+
+
+class LogoutController extends AbstractController {
+
+    public function getContent(): array
+    {
+
+        return [];
+    }
+
+    public function getFileName(): string
+    {
+        return 'logout';
+    }
+
+    public function getPageTitle(): string
+    {
+        return 'Logged out';
+    }
+}
+?>
