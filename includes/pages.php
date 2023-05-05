a31c4e11092d5ef13ec41b3d5cea5b12c1dcb8c9
Adel Benyahia <a.benyahia6993@gmail.com>
2023-05-05 15:57:19 +0200

    v0.1

--- includes/pages.php
+++ includes/pages.php
@@ -4,7 +4,7 @@ use Controller\HomeController;
 use Controller\LoginController;
 use Controller\LogoutController;
 use Controller\MonteurController;
-use Controller\TestController;
+use Controller\ModelController;
 use Model\Page;
 use Service\PagesHandler;
 use Controller\ListPieceController;
@@ -16,6 +16,10 @@ $pagesHandler = new PagesHandler([
     new Page('logout', LogoutController::class),
     new Page('concepteur', ConcepteurController::class),
     new Page('monteur', MonteurController::class),
+<<<<<<< HEAD
     new Page('test', TestController::class),
     new Page('listPiece', ListPieceController::class),
+=======
+    new Page('model', ModelController::class)
+>>>>>>> 2c89e23 (v0.1)
 ]);
