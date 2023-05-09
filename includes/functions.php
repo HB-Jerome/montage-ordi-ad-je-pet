a0a5048a516178a5db4ffd49a07dea0a4fcf3e46
Adel Benyahia <a.benyahia6993@gmail.com>
2023-05-09 10:17:40 +0200

    modification modelHandler __contruct , modification

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,10 @@
 
 function displayOptions(?array $components, $category)
 {
+    ?>
+    <option value=> </option>
+    <?php
+
     $options = array_filter($components, function ($component) use ($category) {
         return $component->GetCategory() == $category;
     });
