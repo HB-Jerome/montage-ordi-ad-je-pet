616afcb5b41761bc1dd2fcd4a8598ed0283135d3
Adel Benyahia <a.benyahia6993@gmail.com>
2023-05-17 09:33:50 +0200

    transformation card model en tableau page list Model

--- src/View/ListModel.php
+++ src/View/ListModel.php
@@ -1,47 +1,70 @@
 <main>
-    <h1 class="text-center">Liste des modèles</h1>
+    <section>
+        <!-- add the filtre of items per category
+         -->
+        <h1 class="text-center">Liste des modèles</h1>
+        <!-- filtre -->
+        <div class=" d-md-flex align-item-center gap-3 ">
+            <!--  filter -->
+            <!-- table liste -->
+            <table id="tableList" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Date Ajouter</th>
+                        <th scope="col">Prix</th>
+                        <th scope="col">Descritpion</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Nombre de modèles créés</th>
+                        <th scope="col">Plus de Détails</th>
+                        <th scope="col">Modifications</th>
+                        <!-- <th scope="col">Disponibilité</th> -->
+                    </tr>
+                </thead>
+                <?php
+                // select itemes from database
+                ?>
+                <tbody>
+                    <!-- show items on the page -->
+                    <?php
 
-    <div class="row my-5 gap-5">
-        <?php foreach ($models as $model) {
-            ?>
-            <div class="card px-5" style="width: 25rem;">
-                <div class="card-body">
-                    <h5 class="card-title text-center py-3">
-                        <?= $model->getName() ?>
-                    </h5>
-                    <p>Decription: </p>
-                    <p class="card-text">
-                        <?= $model->getDescriptionModel() ?>
-                    </p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">type:
-                        <?= $model->getModelType() ?>
-                    </li>
-                    <li class="list-group-item">quantity:
-                        <?= $model->getModelQuantity() ?>
-                    </li>
-                    <li class="list-group-item">price:
-                        <?= $model->getPrice() ?> €
-                    </li>
-                    <li class="list-group-item">isArchived:
-                        <?php if ($model->getIsArchived()) {
-                            echo "true";
-                        } else {
-                            echo "false";
-                        } ?>
-                    </li>
-                    <li class="list-group-item">date d'ajout:
-                        <?= $model->getAddDate() ?>
-                    </li>
-                </ul>
-                <div class="card-body mx-auto">
-                    <a href="?page=updateModel&idModel=<?= $model->getIdModel() ?>" class="card-link">modifier</a>
-                    <a href="?page=detailModel&idModel=<?= $model->getIdModel() ?>" class="card-link">details</a>
-                </div>
-            </div>
-            <?php
-        }
-        ?>
-    </div>
+                    foreach ($models as $model) { ?>
+                        <tr>
+                            <td class="col-1">
+                                <?= $model->getName(); ?>
+
+                            <td class="col-1">
+                                <?= $model->getAddDate(); ?>
+                            </td>
+                            <td class="col-1">
+                                <?= $model->getPrice(); ?>
+                            </td>
+                            <td class="col-3">
+                                <?= $model->getDescriptionModel(); ?>
+                            </td>
+
+                            <td class="col-1">
+                                <?= $model->getModelType(); ?>
+                            </td>
+                            <td class="col-1">
+                                <?= $model->getModelQuantity(); ?>
+                            </td>
+                            <td class="col-1">
+                                <a href="?page=detailsModel&idModel=<?= $model->getIdModel(); ?>"
+                                    class="btn btn-primary">Détails</a>
+                            </td>
+                            <td class="col-1">
+                                <a href="?page=updateModel&idModel=<?= $model->getIdModel(); ?>"
+                                    class="btn btn-primary">Modifier</a>
+                            </td>
+                        </tr>
+
+                        <?php
+                    } ?>
+
+                </tbody>
+            </table>
+        </div>
+
+    </section>
 </main>
