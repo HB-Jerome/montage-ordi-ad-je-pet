3391285c8b0325d7e58592e21c95f446360077ac
komu <komupeter16@yahoo.com>
2023-05-23 16:16:13 +0200

    page monteur page concepteur

--- src/View/concepteur.php
+++ src/View/concepteur.php
@@ -1,22 +1,23 @@
 <section>
     <div class = "d-flex container">
-        <article class = "col-6 bo border border-secondary p-5 m-1">
-            <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Derniers commentaires</h2></a>
+    <article class = "col-6 bo border border-secondary p-5 m-1">
+    <a href="?page=listPiece" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des pièces</h2> </a>
+        </article>
             
         </article>
 
         <article class = "col-6 bo border border-secondary p-5 m-1">
-            <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Modèles et le nombre d'exemplaires montés</h2></a>
+            <a href="?page=listModel" class="link-secondary text-decoration-none"><h2 class = "text-center">Modèles et le nombre d'exemplaires montés</h2></a>
         </article>
     </div>
 
     <div class = "d-flex container mt-5">
-    <article class = "col-6 bo border border-secondary p-5 m-1">
-    <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des pièces</h2> </a>
-        </article>
         <article class = "col-6 bo border border-secondary p-5 m-1">
-        <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des modélès</h2> </a>
+        <a href="?page=listModel" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des modélès</h2> </a>
         </article>
+        
+        <article class = "col-6 bo border border-secondary p-5 m-1">
+            <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Derniers commentaires</h2></a>
     </div>
 
 </section>
