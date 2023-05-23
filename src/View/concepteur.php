b8e388a2b3b60b8aa86da4ec7b7573738fa431b3
komu <komupeter16@yahoo.com>
2023-05-23 15:32:21 +0200

    pour git pull commentaire

--- src/View/concepteur.php
+++ src/View/concepteur.php
@@ -1,9 +1,22 @@
-<main>
-    <h1>Home Concepteur</h1>
-    <section>
-        <h2>list commentaires</h2>
-    </section>
-    <section>
-        <h2>list Modèles</h2>
-    </section>
-</main>
+<section>
+    <div class = "d-flex container">
+        <article class = "col-6 bo border border-secondary p-5 m-1">
+            <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Derniers commentaires</h2></a>
+            
+        </article>
+
+        <article class = "col-6 bo border border-secondary p-5 m-1">
+            <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Modèles et le nombre d'exemplaires montés</h2></a>
+        </article>
+    </div>
+
+    <div class = "d-flex container mt-5">
+    <article class = "col-6 bo border border-secondary p-5 m-1">
+    <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des pièces</h2> </a>
+        </article>
+        <article class = "col-6 bo border border-secondary p-5 m-1">
+        <a href="#" class="link-secondary text-decoration-none"><h2 class = "text-center">Liste complète des modélès</h2> </a>
+        </article>
+    </div>
+
+</section>
